e32c10889ef526b8c6ac57ea99c06b3fcac0a79d
xyouo <88232776+xyouo@users.noreply.github.com>
2026-08-29 12:05:05 +0800

    test: update version check and add start/stop button checks

--- tests/test-module.sh
+++ tests/test-module.sh
@@ -40,7 +40,7 @@ grep -q '^ENV=QlPort=5700$' module/examples/qinglong.conf
 grep -q '^ENV=QlGrpcPort=5501$' module/examples/qinglong.conf
 test ! -e module/examples/cloudflared.conf
 grep -q '^updateJson=https://raw.githubusercontent.com/xyouo/dockroot/main/update.json$' module/module.prop
-grep -q '"version": "v0.8.2"' update.json
+grep -q '"version": "v0.8.3"' update.json
 grep -q '^exec /data/adb/modules/dockroot/bin/drctl ' module/system/bin/drctl
 grep -q '"./kernelsu.js"' module/webroot/app.js
 grep -q 'web-status' module/webroot/app.js
@@ -51,6 +51,8 @@ grep -q 'window.confirm' module/webroot/app.js
 grep -q 'drctl restart' module/webroot/app.js
 grep -q 'data-update' module/webroot/app.js
 grep -q 'data-autostart' module/webroot/app.js
+grep -q 'data-up' module/webroot/app.js
+grep -q 'data-down' module/webroot/app.js
 grep -q 'autostart set' module/webroot/app.js
 grep -q 'drctl update' module/webroot/app.js
 grep -q '自动回滚' module/bin/drctl
